8e2414c50e98877244edcdde378fc9e07fe1096d
Janice Wong <go.janicewong@gmail.com>
2017-09-12 17:01:26 -0400

    First solution

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,36 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
+    if award.name == 'Blue First'
+      if award.expires_in.positive?
+        award.quality < 49 ? award.quality += 1 : award.quality = 50
       else
-        if award.quality < 50
-          award.quality += 1
-        end
+        award.quality < 49 ? award.quality += 2 : award.quality = 50
+      end
+    elsif award.name == 'Blue Compare'
+      if award.expires_in.positive? && award.expires_in <= 5
+        award.quality < 47 ? award.quality += 3 : award.quality = 50
+      elsif award.expires_in.positive? && award.expires_in <= 10
+        award.quality < 48 ? award.quality += 2 : award.quality = 50
+      elsif award.expires_in.positive?
+        award.quality < 49 ? award.quality += 1 : award.quality = 50
+      else
+        award.quality = 0
+      end
+    elsif award.name == 'Blue Star'
+      if award.expires_in.positive?
+        award.quality > 2 ? award.quality -= 2 : award.quality = 0
+      else
+        award.quality > 4 ? award.quality -= 4 : award.quality = 0
+      end
+    elsif award.name != 'Blue Distinction Plus'
+      if award.expires_in.positive?
+        award.quality.positive? ? award.quality -= 1 : award.quality = 0
+      else
+        award.quality > 2 ? award.quality -= 2 : award.quality = 0
       end
     end
+
+    award.expires_in -= 1 if award.name != 'Blue Distinction Plus'
   end
 end
