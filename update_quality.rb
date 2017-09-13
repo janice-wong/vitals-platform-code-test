54b8d0be63f7cc322b9201652b81bbb3a64402c8
Janice Wong <go.janicewong@gmail.com>
2017-09-13 13:55:40 -0400

    Refactored solution

--- update_quality.rb
+++ update_quality.rb
@@ -3,32 +3,27 @@ require 'award'
 def update_quality(awards)
   awards.each do |award|
     if award.name == 'Blue First'
-      if award.expires_in.positive?
-        award.quality < 49 ? award.quality += 1 : award.quality = 50
-      else
-        award.quality < 49 ? award.quality += 2 : award.quality = 50
-      end
+      award.quality += 1 if award.quality < 50
+      award.quality += 1 if award.expires_in <= 0 && award.quality < 50
     elsif award.name == 'Blue Compare'
-      if award.expires_in.positive? && award.expires_in <= 5
-        award.quality < 47 ? award.quality += 3 : award.quality = 50
-      elsif award.expires_in.positive? && award.expires_in <= 10
-        award.quality < 48 ? award.quality += 2 : award.quality = 50
-      elsif award.expires_in.positive?
-        award.quality < 49 ? award.quality += 1 : award.quality = 50
+      if award.expires_in > 0
+        award.quality += 1 if award.quality < 50
+        award.quality += 1 if award.expires_in <= 10 && award.quality < 50
+        award.quality += 1 if award.expires_in <= 5 && award.quality < 50
       else
         award.quality = 0
       end
     elsif award.name == 'Blue Star'
-      if award.expires_in.positive?
-        award.quality > 2 ? award.quality -= 2 : award.quality = 0
+      if award.expires_in > 0
+        award.quality -= 2 if award.quality > 0
       else
-        award.quality > 4 ? award.quality -= 4 : award.quality = 0
+        award.quality -= 4 if award.quality >= 4
       end
     elsif award.name != 'Blue Distinction Plus'
-      if award.expires_in.positive?
-        award.quality.positive? ? award.quality -= 1 : award.quality = 0
+      if award.expires_in > 0
+        award.quality -= 1 if award.quality > 0
       else
-        award.quality > 2 ? award.quality -= 2 : award.quality = 0
+        award.quality -= 2 if award.quality >= 2
       end
     end
 
